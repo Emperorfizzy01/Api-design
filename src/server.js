ae0204e8df39429095072ce60e9e8d6f499d3292
Abidemi Faruk <farugbolahan21@gmail.com>
2021-06-06 09:42:08 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -11,12 +11,23 @@ import listRouter from './resources/list/list.router'
 
 export const app = express()
 
+
+
 app.disable('x-powered-by')
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send({ data: [1, 2, 3] })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send({ message: 'ok'})
+})
 
 app.post('/signup', signup)
 app.post('/signin', signin)
